6e9f93a7a0c1dab63839c7d82d5b0bcc9dfd8f29
Butka <jbutka@vanburenschools.net>
2020-11-18 17:44:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,53 @@
+class Main {
+  public static void main(String[] args) {
+
+    //prompt user for input
+    System.out.println("Hello, please enter your name!");
+
+    //take user input and store it
+
+
+
+
+  }
+
+
+  public void getNameLength(String name){
+
+    //print out how many letters long a name is    
+
+
+  }
+
+
+  public void inCaps(String name){
+
+    //Print out the name in all caps
+
+
+  }
+
+  public void checkForA(String name){
+
+    //print out YES if the name contains at least one a
+
+    //else, print out NO
+
+
+  }
+
+
+  public void nameWithoutFirstLetter(String name){
+
+    //print out the name without the first letter
+
+  }
+
+public void nameWithoutLastLetter(String name){
+
+    //print out the name without the last letter
+
+  }
+
+
+}
